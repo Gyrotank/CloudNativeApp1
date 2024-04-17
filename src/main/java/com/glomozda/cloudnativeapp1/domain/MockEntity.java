2d1dba6855b1a5008a671af712944f88f195a433
KYIV\Dmytro_Glomozda <Dmytro_Glomozda@epam.com>
2024-04-17 20:02:16 +0300

    - initial commit

---
+++ src/main/java/com/glomozda/cloudnativeapp1/domain/MockEntity.java
@@ -0,0 +1,16 @@
+package com.glomozda.cloudnativeapp1.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class MockEntity {
+    Long id;
+
+    String name;
+}
